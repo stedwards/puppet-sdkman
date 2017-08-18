e6f334fb68a92e844dac9d50625273b1f9056978
Paul Bailey <paulba@stedwards.edu>
2017-08-18 12:49:05 -0500

    clean up selfupdater

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,18 +3,10 @@ class sdkman::install(
   $prefix = $sdkman::install::prefix,
   $user   = $sdkman::install::user,
 ) {
-  $_sdkman_init = "source /Users/${::boxen_user}/.sdkman/bin/sdkman-init.sh"
-
-  exec { 'selfupdate-sdkman':
-    command => "bash --login -c '${_sdkman_init} && sdk selfupdate'",
-    onlyif  => "test -e /Users/${::boxen_user}/.sdkman/etc/config",
-    before  => File["${boxen::config::envdir}/sdkmanenv.sh"],
-  }
 
   exec { 'install-sdkman':
     command => 'curl -s "https://get.sdkman.io" | bash',
     creates => "/Users/${::boxen_user}/.sdkman/etc/config",
-    require => Exec['selfupdate-sdkman'],
     before  => File["${boxen::config::envdir}/sdkmanenv.sh"],
   }
 
